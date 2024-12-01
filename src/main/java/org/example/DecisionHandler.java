59c1b2b6ea32152d037b1c564767b4a7910d72ae
Reaz Rahman <reazrahman@tempo.fios-router.home>
2024-12-01 17:40:52 -0500

    backing up

--- src/main/java/org/example/DecisionHandler.java
+++ src/main/java/org/example/DecisionHandler.java
@@ -6,9 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DecisionHandler {
-
-
-
+    
     public DecisionTuple lookForDecision(Map<String, JButton> actionMapper,
                                           Map<String, Boolean> recordKeeper){
 
